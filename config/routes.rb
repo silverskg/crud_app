b848fc1c9e8a4053cf81f1ad6e3e82768fe8fd56
silverskg <chokichoki_gm@i.softbank.jp>
2020-04-20 22:35:36 +0900

    resources_追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # 一覧画面のルーティング
-  get '/users', to: 'users#index'
-#  新規投稿画面
-get '/users/new', to: 'users#new'
-post '/users', to: 'users#create'
-get '/users/:id/edit', to: 'users#edit'
-patch '/users/:id', to: 'users#update'
-delete '/users/:id', to: 'users#destroy'
+# index,new,create,edit,update,destroy,showが扱える
+resources :users
 end
